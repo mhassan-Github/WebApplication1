652b3cc5859baed8837bdb62f7cca48288b67611
mhassan-Github <mohdali.hassan@live.com>
2017-09-21 12:12:48 -0400

    Add project files.

---
+++ WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.DataAccessLayer;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            SalesERPDAL salesDal = new SalesERPDAL();
+            return salesDal.Employees.ToList();
+        }
+    }
+}
